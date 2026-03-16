6a94d1a2a45d81c1f60e1cbe0a83a1321b4311d4
SSeadog <aasssa1225@naver.com>
2026-03-16 20:41:03 +0900

    멀티 서버 연결 주소 변경, AIT 관련 보안 파일 gitignore 등록

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -1,5 +1,6 @@
 public static class Config
 {
     // public static string multiplayEngineServerUrl = "http://localhost:2567";
-    public static string multiplayEngineServerUrl = "http://210.222.104.166:2567";
+    // public static string multiplayEngineServerUrl = "http://210.222.104.166:2567";
+    public static string multiplayEngineServerUrl = "https://seadog.co.kr";
 }
