a38a216831a54d0b5b1bc6251f32bc7e3ad709a1
SSeadog <aasssa1225@naver.com>
2026-03-31 16:17:05 +0900

    멀티 엔진 서버 주소 변경

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -2,5 +2,5 @@ public static class Config
 {
     // public static string multiplayEngineServerUrl = "http://localhost:2567";
     // public static string multiplayEngineServerUrl = "http://210.222.104.166:2567";
-    public static string multiplayEngineServerUrl = "https://seadog.co.kr";
+    public static string multiplayEngineServerUrl = "https://seadog.co.kr/seal-volleyball-multiplay";
 }
